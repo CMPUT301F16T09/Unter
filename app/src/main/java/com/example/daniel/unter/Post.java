aa034a33e5ec8bc3ed80d6df3b6572291ab2dec6
DanielTran3 <dtran3@ualberta.ca>
2016-10-12 21:35:06 -0600

    Test Cases and Class shells

---
+++ app/src/main/java/com/example/daniel/unter/Post.java
@@ -0,0 +1,71 @@
+package com.example.daniel.unter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Daniel on 2016-10-12.
+ */
+public class Post {
+    private String startLocation;
+    private String endLocation;
+    private String status;
+    private String fare;
+    private Rider riderPost;
+    private ArrayList<String> driverOffers;
+
+    public Post(String startLocation, String endLocation, String fare, Rider riderPost) {
+        this.startLocation = startLocation;
+        this.endLocation = endLocation;
+        this.status = "Pending Offer";
+        this.fare = fare;
+        this.riderPost = riderPost;
+    }
+
+    public String getStartLocation() {
+        return startLocation;
+    }
+
+    public void setStartLocation(String startLocation) {
+        this.startLocation = startLocation;
+    }
+
+    public String getEndLocation() {
+        return endLocation;
+    }
+
+    public void setEndLocation(String endLocation) {
+        this.endLocation = endLocation;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public ArrayList<String> getDriveOffers() {
+        return driverOffers;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getFare() {
+        return fare;
+    }
+
+    public void setFare(String fare) {
+        this.fare = fare;
+    }
+
+    public Rider getRiderPost() {
+        return riderPost;
+    }
+
+    public void setRiderPost(Rider riderPost) {
+        this.riderPost = riderPost;
+    }
+
+    public void addDriverOffer(String driverName) {
+        this.driverOffers.add(driverName);
+    }
+}
---
+++ app/src/test/java/com/example/daniel/unter/TestDriver.java
@@ -0,0 +1,12 @@
+package com.example.daniel.unter;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Daniel on 2016-10-12.
+ */
+public class TestDriver extends TestCase {
+    public void testUpdatePost() {
+
+    }
+}
