d48e175fe45293eae24d7e9f24c9efba958f2488
DanielTran3 <dtran3@ualberta.ca>
2016-11-07 12:58:34 -0700

    Daniel's random changes

--- app/src/main/java/com/cmput301f16t09/unter/MainGUIActivity.java
+++ app/src/main/java/com/cmput301f16t09/unter/MainGUIActivity.java
@@ -1,8 +1,11 @@
 package com.cmput301f16t09.unter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 /**
  * The type Main gui activity.
@@ -12,6 +15,18 @@ public class MainGUIActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_profile_ui);
+        setContentView(R.layout.activity_main_gui);
+    }
+
+    public void verifyLoginCredentials(View v) {
+
+        // Verify with UserListOnlineController before changing intent
+        Intent intent = new Intent(MainGUIActivity.this, MainScreenUIActivity.class);
+        startActivity(intent);
+    }
+
+    public void createNewUser() {
+//        Intent intent = new Intent(MainGUIActivity.this, CreateNewUserUIActivity.class);
+//        startActivity(intent);
     }
 }
