d12cae354082f32cda51be11e1989fea3d68a405
AlvinEH <aehuang@ualberta.ca>
2016-11-07 14:48:27 -0700

    Request Details Pre and Post UI Activities

---
+++ app/src/main/java/com/cmput301f16t09/unter/RidersRequestDetailsPreUIActivity.java
@@ -0,0 +1,21 @@
+package com.cmput301f16t09.unter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class RidersRequestDetailsPreUIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riders_request_details_pre_ui);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
