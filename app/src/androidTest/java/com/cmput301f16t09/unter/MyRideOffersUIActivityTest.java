f4c4c1de4a127cb0e87327b8d7f2d3d418db9a77
AlvinEH <aehuang@ualberta.ca>
2016-11-12 21:21:42 -0700

    Two activity tests, temp. Need to check with the non-buggy code

--- app/src/androidTest/java/com/cmput301f16t09/unter/MyRideOffersUIActivityTest.java
+++ app/src/androidTest/java/com/cmput301f16t09/unter/MyRideOffersUIActivityTest.java
@@ -1,21 +1,20 @@
 package com.cmput301f16t09.unter;
 
 import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase;
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.InstrumentationTestRunner;
 import android.util.Log;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.robotium.solo.Solo;
 
 /**
  * Created by alvin on 11/12/2016.
  */
-public class ProvideARideUIActivityTest extends ActivityInstrumentationTestCase2<MainGUIActivity> {
+public class MyRideOffersUIActivityTest extends ActivityInstrumentationTestCase2<MainGUIActivity> {
     private Solo solo;
 
-    public ProvideARideUIActivityTest() {
+    public MyRideOffersUIActivityTest() {
 //        super(com.cmput301f16t09.unter.RequestARideUIActivity.class);
         super(com.cmput301f16t09.unter.MainGUIActivity.class);
     }
@@ -25,21 +24,25 @@ public class ProvideARideUIActivityTest extends ActivityInstrumentationTestCase2
     }
 
     public void setUp() throws Exception {
-        Log.d("TAG6","setUp()");
+        Log.d("TAG7","setUp()");
         solo = new Solo(getInstrumentation(),getActivity());
     }
 
-    public void testProvideARideButtons() {
+    public void testMyRideOfferButtons() {
+        solo.assertCurrentActivity("Wrong Activity", MainGUIActivity.class);
+
         solo.enterText((EditText) solo.getView(R.id.mainScreenUsername), "KappaRoss");
         solo.enterText((EditText) solo.getView(R.id.mainScreenPassword), "RossKappa");
         solo.clickOnButton("Login");
 
         solo.clickOnButton("Request\nA Ride");
-        solo.enterText((EditText) solo.getView(R.id.RequestRideStartLocation), "Bob");
-        solo.enterText((EditText) solo.getView(R.id.RequestRideEndLocation), "Ross");
+        solo.enterText((EditText) solo.getView(R.id.RequestRideStartLocation), "University");
+        solo.enterText((EditText) solo.getView(R.id.RequestRideEndLocation), "The Devil's Den");
+        solo.clickOnButton("Get\nEstimate");
         solo.clickOnButton("CONFIRM");
 
         solo.clickOnButton("My Ride\nRequests");
+        solo.assertCurrentActivity("Wrong Activity",MyRideRequestsUIActivity.class);
         solo.goBack();
 
         solo.goBack();
@@ -54,14 +57,20 @@ public class ProvideARideUIActivityTest extends ActivityInstrumentationTestCase2
         solo.clickInList(0);
         solo.assertCurrentActivity("Wrong Activity", RequestDetailsUIActivity.class);
 
-        solo.clickOnText("kappaross");
-        solo.goBack();
         solo.clickOnButton("Offer Ride");
         solo.waitForText("Successfully sent the offer!");
 
         solo.goBack();
         solo.assertCurrentActivity("Wrong Activity", MainScreenUIActivity.class);
+
+        solo.clickOnButton("My Ride\nOffers");
+        solo.clickInList(0);
+        solo.assertCurrentActivity("Wrong Activity", RideOfferDetailsUIActivity.class);
+
+        solo.goBack();
+        solo.assertCurrentActivity("Wrong Activity", MainScreenUIActivity.class);
         solo.goBack();
         solo.assertCurrentActivity("Wrong Activity", MainGUIActivity.class);
+
     }
 }
